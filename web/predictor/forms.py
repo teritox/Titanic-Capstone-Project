3230bcda54cec5fb7bb8ed5fb1a10ede932b67bd
Caroline-J <caroline.johansson@webexpress.se>
2026-02-06 15:16:36 +0100

    Add placeholders to prediction form

--- web/predictor/forms.py
+++ web/predictor/forms.py
@@ -1,12 +1,12 @@
 from django import forms
 
 class PredictionForm(forms.Form):
-    passenger_class = forms.IntegerField()
-    gender = forms.IntegerField()
+    passenger_class = forms.IntegerField(widget=forms.NumberInput(attrs={'placeholder': '1,2 or 3'}))
+    gender = forms.IntegerField(widget=forms.NumberInput(attrs={'placeholder': 'O for Male, 1 for Female'}))
     age = forms.IntegerField()
     siblings_or_spouses = forms.IntegerField()
     parch = forms.IntegerField()
     ticket_fare = forms.IntegerField()
-    embark_c = forms.IntegerField()
-    embark_q = forms.IntegerField()
-    embark_s = forms.IntegerField()
+    embark_c = forms.IntegerField(widget=forms.NumberInput(attrs={'placeholder': '1 if applies'}))
+    embark_q = forms.IntegerField(widget=forms.NumberInput(attrs={'placeholder': '1 if applies'}))
+    embark_s = forms.IntegerField(widget=forms.NumberInput(attrs={'placeholder': '1 if applies'}))
