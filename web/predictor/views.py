d986f5282d497e2431a458bc1dea1150d76746bd
Caroline-J <caroline.johansson@webexpress.se>
2026-02-06 11:07:24 +0100

    Add prediction form page

--- web/predictor/views.py
+++ web/predictor/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'predictor/home.html')
+
+def prediction_form(request):
+    return render(request, 'predictor/prediction_form.html')
