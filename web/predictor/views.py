504eb2e4ef7c1c8888cd2cd5b59311ce296511d7
Caroline-J <caroline.johansson@webexpress.se>
2026-02-06 13:27:43 +0100

    Add result page

--- web/predictor/views.py
+++ web/predictor/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'predictor/home.html')
+
+def result(request):
+    return render(request, 'predictor/result.html')
