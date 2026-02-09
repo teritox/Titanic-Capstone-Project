13de3a49246cbf1a6c4971503cf1a73411958195
Jiongrui Hu <jiongrui.h@gmail.com>
2026-02-09 15:11:29 +0100

    Add validation/stricts for pclass and gender

--- web/predictor/forms.py
+++ web/predictor/forms.py
@@ -1,8 +1,20 @@
 from django import forms
 
 class PredictionForm(forms.Form):
-    passenger_class = forms.IntegerField(widget=forms.NumberInput(attrs={'placeholder': '1,2 or 3'}))
-    gender = forms.IntegerField(widget=forms.NumberInput(attrs={'placeholder': 'O for Male, 1 for Female'}))
+    passenger_class = forms.TypedChoiceField(choices=[
+        (1, '1st Class'),
+        (2, '2nd Class'),
+        (3, '3rd Class'),
+    ],
+    coerce=int,  # converts string → integer
+    widget=forms.Select)
+    #passenger_class = forms.IntegerField(widget=forms.NumberInput(attrs={'placeholder': '1,2 or 3'}))
+    #gender = forms.IntegerField(widget=forms.NumberInput(attrs={'placeholder': 'O for Male, 1 for Female'}))
+    gender = forms.TypedChoiceField(
+        choices=[(0, 'Male'),(1, 'Female'),],
+        coerce=int,  # converts string → integer
+        widget=forms.Select)
+    
     age = forms.IntegerField()
     siblings_or_spouses = forms.IntegerField()
     parch = forms.IntegerField()
