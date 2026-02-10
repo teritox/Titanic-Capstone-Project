4092a283558c5d97a4e2e53b601cb7d7c74f2d0e
Manasi Patil <manasipatil231195@gmail.com>
2026-02-10 12:02:51 +0100

    Initial commit for adding css to prediction form

--- web/predictor/forms.py
+++ web/predictor/forms.py
@@ -1,12 +1,56 @@
 from django import forms
 
 class PredictionForm(forms.Form):
-    passenger_class = forms.IntegerField(widget=forms.NumberInput(attrs={'placeholder': '1,2 or 3'}))
-    gender = forms.IntegerField(widget=forms.NumberInput(attrs={'placeholder': 'O for Male, 1 for Female'}))
-    age = forms.IntegerField()
-    siblings_or_spouses = forms.IntegerField()
-    parch = forms.IntegerField()
-    ticket_fare = forms.IntegerField()
-    embark_c = forms.IntegerField(widget=forms.NumberInput(attrs={'placeholder': '1 if applies'}))
-    embark_q = forms.IntegerField(widget=forms.NumberInput(attrs={'placeholder': '1 if applies'}))
-    embark_s = forms.IntegerField(widget=forms.NumberInput(attrs={'placeholder': '1 if applies'}))
+
+    PASSENGER_CLASS_CHOICES = [
+        ("", "Select Passenger Class"),
+        (1, "1st Class"),
+        (2, "2nd Class"),
+        (3, "3rd Class"),
+    ]
+
+    GENDER_CHOICES = [
+        ("", "Select Gender"),
+        (0, "Male"),
+        (1, "Female"),
+    ]
+
+    EMBARK_CHOICES = [
+        ("", "Select Embark Location"),
+        ("C", "Cherbourg"),
+        ("Q", "Queenstown"),
+        ("S", "Southampton"),
+    ]
+
+    passenger_class = forms.ChoiceField(
+        choices=PASSENGER_CLASS_CHOICES,
+        widget=forms.Select(attrs={"class": "form-control placeholder-select"})
+    )
+
+    gender = forms.ChoiceField(
+        choices=GENDER_CHOICES,
+        widget=forms.Select(attrs={"class": "form-control placeholder-select"})
+    )
+
+    embark = forms.ChoiceField(
+        choices=EMBARK_CHOICES,
+        widget=forms.Select(attrs={"class": "form-control placeholder-select"})
+    )
+
+    age = forms.IntegerField(
+        widget=forms.NumberInput(attrs={"class": "form-control", "placeholder": "Enter age"})
+    )
+
+    siblings_or_spouses = forms.IntegerField(
+        label="Siblings / Spouses Aboard",
+        widget=forms.NumberInput(attrs={"class": "form-control", "placeholder": "Number of siblings/spouses"})
+    )
+
+    parch = forms.IntegerField(
+        label="Parents / Children Aboard",
+        widget=forms.NumberInput(attrs={"class": "form-control", "placeholder": "Number of parents/children"})
+    )
+
+    ticket_fare = forms.FloatField(
+        widget=forms.NumberInput(attrs={"class": "form-control", "placeholder": "Ticket fare"})
+    )
