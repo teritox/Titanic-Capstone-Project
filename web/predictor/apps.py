e17108c35228408ba9c38102ce7f37e51502de0a
Eneli Elbing <203907107+enelielb@users.noreply.github.com>
2026-02-11 18:40:34 +0100

    Format all Python files with Black

--- web/predictor/apps.py
+++ web/predictor/apps.py
@@ -2,4 +2,4 @@ from django.apps import AppConfig
 
 
 class PredictorConfig(AppConfig):
-    name = 'predictor'
+    name = "predictor"
--- web/predictor/ml_model.py
+++ web/predictor/ml_model.py
@@ -16,6 +16,7 @@ with open(MODEL_PATH, "rb") as f:
 
 # Convert input data from prediction form into model data form
 
+
 def preprocess_data(input_data):
     pclass = input_data["passenger_class"]
     sex = input_data["gender"]
@@ -23,10 +24,10 @@ def preprocess_data(input_data):
     fare = input_data["ticket_fare"]
     family_size = input_data["siblings_or_spouses"] + input_data["parch"] + 1
     embarked = input_data["embark"]
-    
-    #title_map = {"Mr": 0, "Mrs": 1, "Miss": 2, "Master": 3, "Other": 4}
-    #title = title_map.get(input_data["title"], 4)
-    
+
+    # title_map = {"Mr": 0, "Mrs": 1, "Miss": 2, "Master": 3, "Other": 4}
+    # title = title_map.get(input_data["title"], 4)
+
     # add ALL columns exactly as training
     # Right now we have only 3 features in our modeldef preprocess_data(input_data):
     data = {
@@ -36,22 +37,20 @@ def preprocess_data(input_data):
         "Fare": fare,
         "Title": 1,  # TODO change to title after we added the title in prediction form
         "FamilySize": family_size,
-         
     }
 
     # The dataset is Dataframe in our training model, so important to convert to df form
     df = pd.DataFrame([data])
-    return df 
+    return df
 
 
 def prediction(input_data):
     X = preprocess_data(input_data)
-    print(X)  
+    print(X)
     prediction_result = model.predict(X)[0]
     probability = model.predict_proba(X)[0][1]
-    print("The prediction result is:",prediction_result)
-    print("Type of prediction result",type(prediction_result))
+    print("The prediction result is:", prediction_result)
+    print("Type of prediction result", type(prediction_result))
     print("The probability is:", probability)
-    print("Type of the probability",type(probability))
+    print("Type of the probability", type(probability))
     return prediction_result, probability
-  
