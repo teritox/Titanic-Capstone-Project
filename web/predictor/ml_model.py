cefe1a959e968a3fb9905fb1cd81b7ee3dbd5eef
Jiongrui Hu <jiongrui.h@gmail.com>
2026-02-16 09:46:09 +0100

    Test run web with the laste RL model adding pclass one hot encoding

--- web/predictor/ml_model.py
+++ web/predictor/ml_model.py
@@ -5,7 +5,7 @@ import pandas as pd
 
 # Get path for the folder
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-MODEL_PATH = os.path.join(BASE_DIR, "titanic_model_test.pkl")
+MODEL_PATH = os.path.join(BASE_DIR, "titanic_model.pkl")
 
 print(BASE_DIR)
 print(MODEL_PATH)
