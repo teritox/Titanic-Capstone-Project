a955451417d227c3e517dc0439c33a9a46c3eb90
Jiongrui Hu <jiongrui.h@gmail.com>
2026-02-16 09:40:52 +0100

    Remove baseline from all one hot encoded feature in ml_model

--- web/predictor/ml_model.py
+++ web/predictor/ml_model.py
@@ -5,7 +5,7 @@ import pandas as pd
 
 # Get path for the folder
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-MODEL_PATH = os.path.join(BASE_DIR, "titanic_model.pkl")
+MODEL_PATH = os.path.join(BASE_DIR, "titanic_model_test.pkl")
 
 print(BASE_DIR)
 print(MODEL_PATH)
@@ -31,36 +31,41 @@ def preprocess_data(input_data):
     else:
         age_bin = "Senior"
 
-    age_bins = ["Child", "Teen","Adult","Middle Aged", "Senior"]
+    age_bins = ["Teen","Adult","Middle Aged", "Senior"] #Child is the baseline
     agebin_dict = {f"AgeBin_{a}": int(age_bin == a) for a in age_bins}
     
     # Convert embarked input to One Hot encoded Embarked Features
-    embarked_bins = ["Embarked_C", "Embarked_Q", "Embarked_S"]
+    embarked_bins = ["Embarked_C", "Embarked_Q"] #"S" is the baseline
     embarked_dict = {e: int(input_data["embark"] == e) for e in embarked_bins}
     
     #Convert Title input to One Hot encoded Title Features
-    title_bin = ["Master", "Miss","Mrs","Mr","Rare"]
+    title_bin = ["Miss","Mrs","Mr","Rare"] # Master is the baseline
     title_dict = {f"Title_{t}": int(input_data["title"] == t) for t in title_bin}
+
+    #Convert Pclass input to One Hot encoded Pclass Features
+    pclass_bin = [2, 3] # 1 is the baseline
+    pclass_dict = {f"Pclass_{t}": int(input_data["passenger_class"] == t) for t in pclass_bin}
     
     '''
     Order of the features in our model as following:
-        "Sex","Pclass","Fare","FamilySize",
+        "Sex","Fare","FamilySize",
+        "Pclass_2", "Pclass_3",
         "AgeBin_Child","AgeBin_Teen","AgeBin_Adult", "AgeBin_Middle Aged","AgeBin_Senior",
         "Embarked_C","Embarked_Q","Embarked_S",
-        "Title_Master", "Title_Miss","Title_Mrs","Title_Mr","Title_Rare",
+        "Title_Miss","Title_Mrs","Title_Mr","Title_Rare",
     '''
 
     data = {
         "Sex": input_data["gender"],
-        "Pclass": input_data["passenger_class"],
         "Fare": input_data["ticket_fare"],
         "FamilySize": input_data["siblings_or_spouses"] + input_data["parch"] + 1,
+        **pclass_dict,
         **agebin_dict,
         **embarked_dict,
         **title_dict,
               
     }
-
+    print(data)
     # The dataset is Dataframe in our training model, so important to convert to df form
     df = pd.DataFrame([data])
     return df 
